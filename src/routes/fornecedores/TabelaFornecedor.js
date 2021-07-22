9273cc3b4e828e8f18998429192f1a3311c5aff6
dvargas42 <dvargas.eng@gmail.com>
2021-07-22 16:36:46 -0300

    feat: create Sequelize translations

--- src/routes/fornecedores/TabelaFornecedor.js
+++ src/routes/fornecedores/TabelaFornecedor.js
@@ -1,7 +1,34 @@
 const Modelo = require("./ModeloTabelaFornecedor");
+const CustomError = require("../../errors/CustomError");
 
 module.exports = {
   listar() {
     return Modelo.findAll();
+  },
+
+  inserir(fornecedor) {
+    return Modelo.create(fornecedor);
+  },
+
+  async buscarPorId(id) {
+    const encontrado = await Modelo.findOne({ where: { id: id } });
+
+    if (!encontrado) {
+      throw new CustomError("Fornecedor não encontrado!", 404);
+    }
+
+    return encontrado;
+  },
+
+  atualizar(id, dados) {
+    return Modelo.update(dados, {
+      where: { id: id },
+    });
+  },
+
+  remover(id) {
+    return Modelo.destroy({
+      where: { id: id },
+    });
   },
 };
