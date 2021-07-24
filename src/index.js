a5f5a49fb6cc450c94335d9e074d97f28c2b59f8
dvargas42 <dvargas.eng@gmail.com>
2021-07-24 15:09:24 -0300

    feat: create error route

--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const config = require("config");
 
-const route = require("./routes/fornecedores");
+const CustomError = require("./errors/CustomError");
+const routeFornecedores = require("./routes/fornecedores");
 
 const app = express();
 
-app.use(bodyParser.json());
+app.use(express.json());
 
-app.use("/api/fornecedores", route);
+app.use("/api/fornecedores", routeFornecedores);
+
+app.use((error, request, response, next) => {
+  let status = 500;
+  let message = "Internal server error!";
+
+  if (error instanceof CustomError) {
+    status = error.statusCode;
+    message = error.message;
+  }
+
+  response.status(status);
+  response.send(JSON.stringify({ message, status }));
+});
 
 app.listen(config.get("api.port"), () =>
   console.log(`A API está rodando na porta ${config.get("api.port")}!`)
