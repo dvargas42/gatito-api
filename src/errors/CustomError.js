5dca9906b8b2ad71af41addb6d39f4bbf2122be7
dvargas42 <dvargas.eng@gmail.com>
2021-07-30 14:09:05 -0300

    feat: implement attribute id

--- src/errors/CustomError.js
+++ src/errors/CustomError.js
@@ -1,7 +1,8 @@
 class CustomError extends Error {
-  constructor(message, statusCode) {
+  constructor(message, statusCode, id = 0) {
     super(message);
     this.statusCode = statusCode;
+    this.id = id;
   }
 }
 
